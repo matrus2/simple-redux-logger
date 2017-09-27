3f5f968c7ef9c97841604e4807a3ecf3b507c3ca
Bogusz Przybyslawski <bogusz.przybyslawski@sofomo.eu>
2017-09-27 16:21:18 +0200

    Add options doStringify and doLogClient, update readme

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ export default function (initialState = {}) {
   if (process.env.NODE_ENV === 'development') {
     const logger = require('simple-redux-logger'); // eslint-disable-line global-require
 
-    middlewares.push(logger);
+    middlewares.push(logger());
   }
 
 
@@ -22,3 +22,21 @@ export default function (initialState = {}) {
     applyMiddleware(...middlewares));
 }
 ```  
+
+#### Options
+
+  **`doStringify`** - it is for server logging to show entire state. Default is _true_ .If you would like to shrink the output set to _false_ 
+  
+  **`doLogClient`** - default _false_, if you don't use `redux-devtools-extension` for chrome you can set this options to _true_
+
+######Usage with options:
+
+```javascript
+    middlewares.push(logger({ doStringify: false }));
+
+```
+
+
+####License
+
+MIT
--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-const createLogger = store => next => (action) => {
+const defaultOptions = {
+  doStringify: true,
+  doLogClient: false,
+};
+
+
+const createLogger = (options = {}) => ({ getState, options = {} }) => next => (action) => {
   let result;
-  if (typeof window !== 'undefined') {
+  options = Object.assign({}, defaultOptions, options);
+  if (typeof window !== 'undefined' && options.doLogClient) {
     console.info('%cdispatching', 'color:blue', action.type); // eslint-disable-line no-console
     result = next(action);
-    console.log('%cnext state', 'color:blue', store.getState()); // eslint-disable-line no-console
-  } else {
-    console.info('dispatching \x1b[32m', action.type, '\x1b[0m'); // eslint-disable-line no-console
+    console.log('%cnext state', 'color:blue', getState()); // eslint-disable-line no-console
+  } else if (typeof window === 'undefined') {
+    console.info('dispatching \x1b[31m', action.type, '\x1b[0m'); // eslint-disable-line no-console
     result = next(action);
-    const newState = store.getState();
-    console.log('\x1b[35m', JSON.stringify(newState, 2, 2), '\x1b[0m:');
+    const newState = getState();
+    if (options.doStringify) {
+      console.log('\x1b[37m', JSON.stringify(newState, 2, 2), '\x1b[0m:');
+    } else {
+      console.log('\x1b[37m', newState, '\x1b[0m:');
+    }
   }
   return result;
 };
